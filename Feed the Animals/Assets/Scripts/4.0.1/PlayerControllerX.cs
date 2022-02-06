b961e60291182479a2681e3a0b08373871f85cf5
r-bjorn <ravidtrivedi111@gmail.com>
2022-02-06 00:56:22 -0500

    Adding Scripts
    Adding scripts for level 4.0.1

---
+++ Feed the Animals/Assets/Scripts/4.0.1/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private GameObject dogInstance;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press and if no more dogs on scene, new instance
+        if (Input.GetKeyDown(KeyCode.Space) && !dogInstance)
+        {
+            dogInstance = Instantiate(
+                dogPrefab, 
+                transform.position, 
+                dogPrefab.transform.rotation);
+        }
+    }
+}
